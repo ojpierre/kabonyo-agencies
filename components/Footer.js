f58b50116c557377486afc1732e5c6f935291387
Peter Ouma <pierreouma@gmail.com>
2024-07-25 14:24:42 +0300

    img edits for 1st vercel deployment

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,12 @@
-import Image from "next/image";
-
 const Footer = () => (
   <footer className="py-8 bg-[var(--card)] text-[var(--card-foreground)]">
     <div className="max-w-4xl mx-auto text-center">
-      <Image
-        width={220} // Provide the width
-        height={48}
-        src="/assets/images/kabonyoagencieslogo.png"
+      <img
+        src="/assets/images/kabonyoagencieslogo.PNG"
         alt="Company Logo"
         className="h-10 mx-auto mb-4"
       />
+
       <p className="mt-4">&copy; 2024 Kabonyo Agencies. All rights reserved.</p>
     </div>
   </footer>
